90c09441835b1b1869bb95459c3867f6c26af9c8
MAXIMATOR <dedgenimatios@gmail.com>
2023-11-04 12:01:39 +0430

    Version 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Human:
     def __init__(self, name="Human", job=None, home=None, car=None):
         self.name = name
@@ -9,11 +10,12 @@ class Human:
         self.job = job
         self.home = home
         self.car = car
+
     def get_home(self):
         self.home = House()
 
     def get_car(self):
-        self.home = Auto(brands_of_car)
+        self.car = Auto(brands_of_car)
 
     def get_job(self):
         if self.car.drive():
@@ -23,9 +25,135 @@ class Human:
             return
         self.job = Job(job_list)
 
+    def to_repair(self):
+        self.car.strength += 100
+        self.money -= 50
+
+    def clean_home(self):
+        self.gladness -= 5
+        self.home.mess += 0
+
+    def chill(self):
+        self.gladness += 10
+        self.home.mess += 5
+
     def eat(self):
-        if self.home.food <=0:
-            self.shoping("food")
+        if self.home.food <= 0:
+            self.shopping("food")
+        else:
+            if self.satiety >= 100:
+                self.satiety = 100
+                return
+            self.satiety += 5
+            self.home.food -= 5
+
+    def work(self):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                self.shopping("fuel")
+                return
+            else:
+                self.to_repair()
+                return
+        self.money += self.job.salary
+        self.gladness -= self.job.salary
+        self.satiety -= 4
+
+    def shopping(self, manage):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                manage = 'fuel'
+            else:
+                self.to_repair()
+                return
+        if manage == "fuel":
+            print("I bought fuel!")
+            self.money -= 100
+            self.car.fuel += 100
+        elif manage == 'food':
+            print("Bought food!")
+            self.money -= 50
+            self.home.food += 50
+        elif manage == "delicacies":
+            print('I am happy! Delicacies')
+            self.gladness += 10
+            self.satiety += 2
+            self.money -= 15
+
+    def days_indexes(self, day):
+        day = f" Today the {day} of {self.name}'s life "
+        print(f"{day:=^50}", "\n")
+        human_indexes = self.name + "'s indexes"
+        print(f"{human_indexes:^50}", "\n")
+        print(f"Money – {self.money}")
+        print(f"Satiety – {self.satiety}")
+        print(f"Gladness – {self.gladness}")
+        home_indexes = "Home indexes"
+        print(f"{home_indexes:^50}", "\n")
+        print(f"Food – {self.home.food}")
+        print(f"Mess – {self.home.mess}")
+        car_indexes = f"{self.car.brand} car indexes"
+        print(f"{car_indexes:^50}", "\n")
+        print(f"Fuel – {self.car.fuel}")
+        print(f"Strength – {self.car.strength}")
+
+    def is_alive(self):
+        if self.gladness <= 0:
+            print('Depression...')
+            return False
+        if self.satiety < 0:
+            print('Dead...')
+        if self.money < - 500:
+            print('Bankrupt...')
+            return False
+
+    def live(self, day):
+        if self.is_alive() == False:
+            return False
+        if self.home is None:
+            print('Settled in the house!')
+            self.get_home()
+        if self.car is None:
+            self.get_car()
+            print(f'I bought a car {self.car.brand}')
+        if self.job is None:
+            self.get_job()
+            print(f"I don't have a job, gping to get a job {self.job.job}"
+                  f"with salary {self.job.salary}")
+
+        self.days_indexes(day)
+        dice = random.randint(1, 4)
+        if self.satiety < 20:
+            print("I'll go eat!")
+            self.eat()
+        elif self.gladness < 20:
+            if self.home.mess > 15:
+                self.home.clean()
+            else:
+                print("Tim's chill")
+                self.chill()
+        elif self.money < 0:
+            print('Start working!')
+            self.work()
+        elif self.car.strength < 15:
+            print("I need to repair my car!")
+            self.to_repair()
+        if dice == 1:
+            print("Let's chill!")
+            self.chill()
+        if dice == 2:
+            print('Start working!')
+            self.work()
+        if dice == 3:
+            print('Cleaning time!')
+            self.clean_home()
+        if dice == 4:
+            print("Time for treats")
+            self.shopping(manage='delicacies')
 
 
 class House:
@@ -33,6 +161,7 @@ class House:
     def __init__(self):
         self.mess = 0
         self.food = 0
+
 
 class Auto:
     def __init__(self, brand_list):
@@ -50,27 +179,29 @@ class Auto:
             print("The car can't move!")
             return False
 
+
 class Job:
     def __init__(self, job_list):
         self.job = random.choice(list(job_list))
         self.salary = job_list[self.job]['salary']
         self.gladness_less = job_list[self.job]['gladness_less']
 
+
 job_list = {
     "Java developer": {"salary": 50, "gladness_less": 10},
     "Python developer": {"salary": 40, "gladness_less": 3},
     "C++ developer": {"salary": 60, "gladness_less": 25},
     "Rust developer": {"salary": 70, "gladness_less": 15},
-    }
-
-
-
-brands_of_car = {
-    "BMW":{"fuel": 100, "strength": 100, "consumption": 6},
-    "Lada":{"fuel": 50, "strength": 40, "consumption": 10},
-    "Volvo":{"fuel": 80, "strength": 150, "consumption": 8},
-    "Ferrari":{"fuel": 80, "strength": 120, "consumption": 14}
 }
 
-h = Human("Aleks")
-h.get_car()
+brands_of_car = {
+    "BMW": {"fuel": 100, "strength": 100, "consumption": 6},
+    "Lada": {"fuel": 50, "strength": 40, "consumption": 10},
+    "Volvo": {"fuel": 80, "strength": 150, "consumption": 8},
+    "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14}
+}
+
+nick = Human(name="Nick")
+for day in range(1, 365):
+    if nick.live(day) == False:
+        break
